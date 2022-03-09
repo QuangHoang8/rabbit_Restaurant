f9b0acc5a862feabcb78678db99fd81c51ca4b17
Bùi Hoàng <hoangbq@fanxipan.net>
2022-03-09 16:23:26 +0700

    Add chef, menu, intro, laurels

--- src/container/Intro/Intro.jsx
+++ src/container/Intro/Intro.jsx
@@ -1,11 +1,45 @@
-import React from 'react';
+import React, { useState } from "react";
 
-import './Intro.css';
+import { BsFillPlayFill, BsPauseFill } from "react-icons/bs";
+import { meal } from "../../constants";
+import "./Intro.css";
 
-const Intro = () => (
-  <div>
-    Intro
-  </div>
-);
+const Intro = () => {
+  const vidRef = React.useRef();
+  const [playVideo, setPLayVideo] = useState(false);
+
+  const handleVideo = () => {
+    setPLayVideo((prevPlayVideo) => !prevPlayVideo);
+    if (playVideo) {
+      vidRef.current.pause();
+    } else {
+      vidRef.current.play();
+    }
+  };
+  return (
+    <div className="app__video" onClick={handleVideo}>
+      <video
+        src={meal}
+        type="video/mp4"
+        ref={vidRef}
+        loop
+        controls={false}
+        muted
+      />
+      <div className="app__video-overlay flex__center">
+        <div
+          className="app__video-overlay_circle flex__center"
+          // onClick={handleVideo}
+        >
+          {playVideo ? (
+            <BsPauseFill color="#fff" fontSize={30} />
+          ) : (
+            <BsFillPlayFill color="#fff" fontSize={30} />
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Intro;
